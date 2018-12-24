2d305b500f9815a9cee94f411c813ec70da9e00c
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-24 10:57:15 +0300

    Initial Commit

---
+++ Detect-a-Beacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Detect-a-Beacon
+//
+//  Created by Evgeniy Ryshkov on 24/12/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
